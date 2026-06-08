f8e60b41e6f18566c4f66dab1e3df5bed4362b82
lunaHieu <nguyenvanhieu040205@gmail.com>
2026-06-08 09:30:34 +0700

    delete hardcode

--- src/main/java/com/example/medbook/controller/AdminController.java
+++ src/main/java/com/example/medbook/controller/AdminController.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
--- src/main/java/com/example/medbook/controller/AuthController.java
+++ src/main/java/com/example/medbook/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
